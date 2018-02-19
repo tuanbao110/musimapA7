8daa772aaab7ffe9f2287140f26520827fbe0670
Tuan Bao <tbao@ucsd.edu>
2018-02-19 11:34:56 -0800

    Like this page, data updates

--- routes/likeThis.js
+++ routes/likeThis.js
@@ -11,8 +11,29 @@ exports.view = function(req, res){
 
 exports.viewFromArtist = function(req, res) {
     var fromName = req.params.name;
+    var fromGenre = "";
+    //var likeData = JSON.parse(JSON.stringify(data));
+    var likeData = {"artists": []};
 
-    var likeData = JSON.parse(JSON.stringify(data));
+    //get genre of from artist
+    for(var i = 0; i < data.artists.length; i++) {
+        var obj = data.artists[i];
+
+        if(obj.name == fromName) {
+            fromGenre = obj.genre;
+        }
+    }
+
+    //get artists of same genre
+    for(var i = 0; i < data.artists.length; i++) {
+        var obj = data.artists[i];
+
+        if(obj.genre == fromGenre) {
+            console.log("Found genre match: " + obj.name);
+            likeData.artists.push(obj);
+        }
+    }
+    
 
     //remove 'from artist' from like this data
     for(var i = 0; i < likeData.artists.length; i++) {
@@ -24,5 +45,5 @@ exports.viewFromArtist = function(req, res) {
         }
     }
 
-    res.render('likeThis', likeData);
+    res.render('likeThis', {fromName, likeData});
 };
